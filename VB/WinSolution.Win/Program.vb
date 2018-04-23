9551ab03498ea64787f48cd6eed235c2498e664d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WinSolution.Win/Program.vb
@@ -0,0 +1,33 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Configuration
+Imports System.Windows.Forms
+Imports DevExpress.ExpressApp.Security
+
+Namespace WinSolution.Win
+	Friend NotInheritable Class Program
+		''' <summary>
+		''' The main entry point for the application.
+		''' </summary>
+		Private Sub New()
+		End Sub
+		<STAThread> _
+		Shared Sub Main()
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached
+			Dim winApplication As New WinSolutionWindowsFormsApplication()
+			If ConfigurationManager.ConnectionStrings("ConnectionString") IsNot Nothing Then
+				winApplication.ConnectionString = ConfigurationManager.ConnectionStrings("ConnectionString").ConnectionString
+			End If
+			Try
+    DevExpress.ExpressApp.Xpo.InMemoryDataStoreProvider.Register()
+    				winApplication.ConnectionString = DevExpress.ExpressApp.Xpo.InMemoryDataStoreProvider.ConnectionString
+				winApplication.Setup()
+				winApplication.Start()
+			Catch e As Exception
+				winApplication.HandleException(e)
+			End Try
+		End Sub
+	End Class
+End Namespace
